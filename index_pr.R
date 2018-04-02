48ce5d110b928a03ccd90848bbd38b0d01994428
Prita An <prita.an@gmail.com>
2018-04-02 18:36:13 -0400

    Modify RMarkdown

--- index_pr.R
+++ index_pr.R
@@ -6,6 +6,7 @@ library(ggplot2)
 library(dbplyr)
 library(RSQLite)
 library(DBI)
+library (plyr)
 
 #load data from combined.csv
 surveys_combined <- read.csv("data/combined.csv")
@@ -17,6 +18,39 @@ surveys_combined_clear<- surveys_combined %>% filter(!is.na(sex),
                                                          hindfoot_length != "",
                                                          !is.na(weight), 
                                                          weight != "") 
+
+weight_control <- surveys_combined_clear %>% filter(plot_type == 'Control') %>% select(weight)
+weight_ltk_exclosure <- surveys_combined_clear %>% filter(plot_type == 'Long-term Krat Exclosure') %>% select(weight)
+
+mat=matrix(data=c(weight_control, weight_ltk_exclosure),ncol=2)
+
+l=list(control = weight_control, ltx_exclousure = weight_ltk_exclosure)
+ldata = do.call(rbind.data.frame, l)
+df <- ldply (l, data.frame)
+
+mean_weight <- surveys_combined_clear %>% group_by(plot_type, genus) %>% 
+  select(plot_type, genus, species_id, weight)
+
+surveys_wide_mean_weight <- surveys_mean_weight %>%
+  spread(key = genus, value = mean_weight, fill = 0)
+
+
+
+ggplot(data = surveys_combined_clear) + 
+  geom_point(mapping = aes(x=plot_type, y = weight))
+
+ggscatter(surveys_combined_clear, x = "plot_type", y = "weight",title = "Relationship between weight and plot type", xlab = "Plot Type", ylab = "weight(g)",
+          add = "reg.line",  # Add regressin line
+          add.params = list(color = "blue", fill = "lightgray"), # Customize reg. line
+          conf.int = TRUE # Add confidence interval
+)
+
+stat_cor(method = "pearson", label.x = 10, label.y = 300)
+
+
+
+
+
 
 #create variable to show weight in plot type : control
 weight_control <- surveys_combined_clear %>% filter(plot_type == 'Control') %>% select(weight)
